2de8e5cd80b889e3ef01122bacdebaed34aa7a82
Joseph Abraham <ekurubal24@gmail.com>
2021-01-16 17:58:40 +0300

    added related_name

--- src/quiz/serializers.py
+++ src/quiz/serializers.py
@@ -1,6 +1,5 @@
-from django.db.models import fields
 from rest_framework import serializers
-from .models import Category, Quiz
+from .models import Answer, Category, Question, Quiz
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -20,3 +19,28 @@ class CategoryDetailSerializer(serializers.ModelSerializer):
             "title",
             "question_count",
         )
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model = Answer
+        fields = [
+            'id',
+            'answer_text',
+            'is_right',
+        ]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = (
+            "title",
+            "answer",
+            "difficulty",
+        )
+
+        
--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Category, Quiz
-from .serializers import CategorySerializer, CategoryDetailSerializer
+from .models import Category, Quiz, Question
+from .serializers import CategorySerializer, CategoryDetailSerializer, QuestionSerializer
 
 
 class CategoryList(generics.ListAPIView):
@@ -17,3 +17,13 @@ class CategoryDetail(generics.ListAPIView):
         category = self.kwargs["category"]  # backend, frontend
         queryset = queryset.filter(category__name=category)
         return queryset
+
+
+class QuizDetail(generics.ListAPIView):
+    serializer_class = QuestionSerializer
+    
+    def get_queryset(self):
+        queryset = Question.objects.all()
+        title = self.kwargs["title"]
+        queryset = queryset.filter(quiz_title=title)
+        return queryset    
