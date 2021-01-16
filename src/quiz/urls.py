f3ed48b570b61010f6a8e7485d6a01ffa67b82ac
Joseph Abraham <ekurubal24@gmail.com>
2021-01-16 18:44:10 +0300

    created users_app

--- src/quiz/urls.py
+++ src/quiz/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CategoryList, CategoryDetail, QuizDetail
 
 urlpatterns = [
-    path("category/", CategoryList.as_view(), name="category"),
-    path("category/<category>/", CategoryDetail.as_view(), name="category-detail"),
+    path("", CategoryList.as_view(), name="category"),
+    path("<category>/", CategoryDetail.as_view(), name="category-detail"),
     path("question/<title>/", QuizDetail.as_view(), name="question")
 ]
 
